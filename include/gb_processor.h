63db70d676f617a5a63e5cfca45ec5f5be48ad54
mertbasaryildirim <mertbasaryildirim@gmail.com>
2025-11-29 13:07:05 +0300

    update processor struct

--- include/gb_processor.h
+++ include/gb_processor.h
@@ -5,7 +5,25 @@
 
 typedef struct
 {
+    struct
+    {
+        uint8_t a;
+        uint8_t f;
+        uint8_t b;
+        uint8_t c;
+        uint8_t d;
+        uint8_t e;
+        uint8_t h;
+        uint8_t l;
+    } registers;
+
     uint16_t pc;
+    uint16_t sp;
+    uint8_t ime; /* interrupt master enable */
+    uint8_t halted;
+    uint8_t stopped;
+    uint64_t cycles;
+
 } GB_PROC;
 
 extern GB_PROC gb_proc;
