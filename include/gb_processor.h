ba2498455fa9491d4de8ce043f5dc829195d93a7
mertbasaryildirim <mertbasaryildirim@gmail.com>
2025-11-29 15:29:44 +0300

    implement OP_0b00xx0001 and optimize processor struct for clean code

--- include/gb_processor.h
+++ include/gb_processor.h
@@ -5,20 +5,37 @@
 
 typedef struct
 {
-    struct
+    union
     {
-        uint8_t a;
-        uint8_t f;
-        uint8_t b;
-        uint8_t c;
-        uint8_t d;
-        uint8_t e;
-        uint8_t h;
-        uint8_t l;
+        struct
+        {
+            uint8_t f; /* AF low  */
+            uint8_t a; /* AF high */
+
+            uint8_t c; /* BC low  */
+            uint8_t b; /* BC high */
+
+            uint8_t e; /* DE low  */
+            uint8_t d; /* DE high */
+
+            uint8_t l; /* HL low  */
+            uint8_t h; /* HL high */
+
+            uint8_t spl; /* SP low  */
+            uint8_t sph; /* SP high */
+        } r8;
+        struct
+        {
+            uint16_t af;
+            uint16_t bc;
+            uint16_t de;
+            uint16_t hl;
+            uint16_t sp;
+        } r16;
     } registers;
 
+    uint8_t opcode;
     uint16_t pc;
-    uint16_t sp;
     uint8_t ime; /* interrupt master enable */
     uint8_t halted;
     uint8_t stopped;
