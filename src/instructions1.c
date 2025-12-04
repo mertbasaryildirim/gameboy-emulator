17b506f05c5ed96de55c0658757b1898caedbbfa
mertbasaryildirim <mertbasaryildirim@gmail.com>
2025-12-04 21:47:28 +0300

    implement OP_0b10010xxx instructions

--- src/instructions1.c
+++ src/instructions1.c
@@ -557,3 +557,25 @@ void OP_0b10001110(void)
 
     gb_proc.cycles += 2;
 }
+
+void OP_0b10010xxx(void)
+{
+    uint8_t src_idx = gb_proc.opcode & 0x07u;
+    uint8_t a = gb_proc.registers.r8.a;
+    uint8_t value = *reg8_table[src_idx];
+
+    uint8_t result = (uint8_t)(a - value);
+
+    GB_FLAG_CLEAR(GB_FLAG_Z | GB_FLAG_H | GB_FLAG_C);
+    GB_FLAG_SET(GB_FLAG_N);
+
+    if ((a & 0x0Fu) < (value & 0x0Fu))
+        GB_FLAG_SET(GB_FLAG_H);
+    if (a < value)
+        GB_FLAG_SET(GB_FLAG_C);
+    if (result == 0)
+        GB_FLAG_SET(GB_FLAG_Z);
+
+    gb_proc.registers.r8.a = result;
+    gb_proc.cycles += 1;
+}
